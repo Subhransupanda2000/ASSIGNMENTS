cc233f12253472d8a1715908f8cadef489db0ad7
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-21 19:16:22 +0530

    GetValue LinkedList

---
+++ src/main/java/Array/GetValueLinkedList.java
@@ -0,0 +1,66 @@
+package Array;
+
+import java.io.*;
+
+public class GetValueLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public int getAt(int idx) {
+            if (size == 0) {
+                System.out.println("List is empty");
+                return -1;
+            } else if (idx < 0 || idx >= size) {
+                System.out.println("Invalid arguments");
+                return -1;
+            } else {
+                Node temp = head;
+                for (int i = 0; i < idx; i++) {
+                    temp = temp.next;
+                }
+                return temp.data;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+        while (!str.equals("quit")) {
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("getAt")) {
+                int idx = Integer.parseInt(str.split(" ")[1]);
+                int val = list.getAt(idx);
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            }
+            str = br.readLine();
+        }
+    }
+}
