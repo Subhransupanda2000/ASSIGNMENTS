bcc42e722adf24dee6c8d7dcc7a08352ba269447
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-22 09:04:22 +0530

    ReverseLinkedList

---
+++ src/main/java/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,91 @@
+package LinkedList;
+
+import java.io.*;
+
+import java.util.*;
+
+public class ReverseLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void display() {
+            for (Node temp = head; temp != null; temp = temp.next) {
+                System.out.print(temp.data + " ");
+            }
+            System.out.println();
+        }
+
+
+
+        private Node getNodeAt(int idx) {
+            Node temp = head;
+            for (int i = 0; i < idx; i++) {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void reverseDI() {
+            int li = 0;
+            int ri = size - 1;
+
+            while (li < ri) {
+                Node left = getNodeAt(li);
+                Node right = getNodeAt(ri);
+
+                int temp = left.data;
+                left.data = right.data;
+                right.data = temp;
+
+                li++;
+                ri--;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("size")) {
+                System.out.println(list.size());
+            } else if (str.startsWith("display")) {
+                list.display();
+            } else if (str.startsWith("reverseDI")) {
+                list.reverseDI();
+            }
+            str = br.readLine();
+        }
+    }
+}
