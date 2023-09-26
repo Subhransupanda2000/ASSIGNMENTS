0d22f4c3f07a94e8fabd8bc365357717d1213316
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-26 20:02:01 +0530

    IteratorExample

---
+++ src/main/java/Collection/IteratorExample.java
@@ -0,0 +1,18 @@
+package Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class IteratorExample {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String fruit = iterator.next();
+            System.out.println(fruit);
+        }
+    }
+}
