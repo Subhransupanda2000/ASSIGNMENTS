3543ed461bf26607060ae2fdfac1d6bacdd9a64f
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2023-09-27 20:07:57 +0530

    IdentityHashMap

---
+++ src/main/java/Collection/IdentityHashMapDemo.java
@@ -0,0 +1,15 @@
+package Collection;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+
+class IdentityHashMapDemo {
+public static void main(String[] args) {
+        IdentityHashMap m = new IdentityHashMap();
+        Integer I1 = new Integer(10);
+        Integer I2 = new Integer(10);
+        m.put(I1,"Pawan");
+        m.put(I2,"Kalyan");
+        System.out.println(m); //{10=Kalyan}
+        }
+        }
